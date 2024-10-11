d3c9a6570df89550ce5ac34ae1253e85296ac123
Lorenzo-alt <lorenzo.juliati@terra.com.br>
2024-10-11 00:06:41 -0300

    feat: Resolvendo desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123123123", "Modelo 1", "22222", 123);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\nSmartphone Iphone:");
+Smartphone iphone = new Iphone("213123", "Modelo 1", "23121234", 250);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
